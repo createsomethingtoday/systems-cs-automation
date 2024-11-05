50dbd704b7b173b1da3d4493e0a027c00272ee4a
Create Something <micah@createsomething.io>
2024-11-05 00:40:09 -0600

    Update logo view to allow correct logo rendering

--- packages/editor-ui/src/components/Logo.vue
+++ packages/editor-ui/src/components/Logo.vue
@@ -7,17 +7,28 @@ import { defineComponent } from 'vue';
 import { mapStores } from 'pinia';
 import { useRootStore } from '@/stores/n8nRoot.store';
 import { useUIStore } from '@/stores/ui.store';
+import { useRoute } from 'vue-router';
 
 export default defineComponent({
+	setup() {
+		const route = useRoute();
+		return { route };
+	},
 	computed: {
 		...mapStores(useRootStore, useUIStore),
 		basePath(): string {
 			return this.rootStore.baseUrl;
 		},
 		logoPath(): string {
-			const path = this.basePath + this.uiStore.logo;
-			console.log('Full logo path:', path);
-			return path;
+			// Check if we're on the signin page
+			const isSignInPage = this.route.path === '/signin';
+			if (isSignInPage) {
+				const logoName = process.env.VUE_APP_LOGO_NAME || 'halfdozen';
+				const suffix = this.uiStore.appliedTheme === 'dark' ? '-dark.svg' : '.svg';
+				return `${this.basePath}static/logo/channel/${logoName}${suffix}`;
+			}
+
+			return this.basePath + this.uiStore.logo;
 		},
 	},
 });

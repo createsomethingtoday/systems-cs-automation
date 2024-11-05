a69f20c9596fba5452e00245161062bd1cb18190
Create Something <micah@createsomething.io>
2024-11-05 02:07:41 -0600

    Return the design system

--- packages/editor-ui/src/main.ts
+++ packages/editor-ui/src/main.ts
@@ -2,8 +2,8 @@ import { createApp } from 'vue';
 
 import 'vue-json-pretty/lib/styles.css';
 import '@jsplumb/browser-ui/css/jsplumbtoolkit.css';
-// import 'n8n-design-system/css/index.scss';
-import 'n8n-design-system/css/tailwind/index.css';
+import 'n8n-design-system/css/index.scss';
+// import 'n8n-design-system/css/tailwind/index.css';
 
 import './n8n-theme.scss';
 
